dc79517f6412988a6a5cadbd0654ef9e397b5c1c
David Schwartz <davidschwartz23@gmail.com>
2019-07-08 18:34:00 -0700

    Changed table structures and database name

--- models/jobs.js
+++ models/jobs.js
@@ -1,16 +1,12 @@
-"use strict";
+'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Jobs = sequelize.define(
-    "Jobs",
-    {
-      job_name: DataTypes.STRING,
-      job_length: DataTypes.STRING,
-      user_type: DataTypes.STRING,
-      paid_unpaid: DataTypes.BOOLEAN,
-      pay_amount: DataTypes.STRING
-    },
-    {}
-  );
+  const Jobs = sequelize.define('Jobs', {
+    job_name: DataTypes.STRING,
+    job_length: DataTypes.STRING,
+    user_type: DataTypes.STRING,
+    paid_unpaid: DataTypes.STRING,
+    pay_amount: DataTypes.STRING
+  }, {});
   Jobs.associate = function(models) {
     // associations can be defined here
   };
