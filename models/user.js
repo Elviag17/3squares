20f3b1048d1c9d4e66abc5d050ef63735f47bdcf
tarql24 <tarql24@gmail.com>
2019-07-08 21:53:02 -0700

    compare code-DONT MERGE

--- models/user.js
+++ models/user.js
@@ -1,16 +1,44 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    address: DataTypes.STRING,
-    living_situation: DataTypes.BOOLEAN,
-    job_situation: DataTypes.BOOLEAN
-  }, {});
+var bcrypt = require("bcrypt");
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    // Hooks are automatic methods that run during various phases of the User Model lifecycle
+    // In this case, before a User is created, we will automatically hash their password
+    hooks: {
+      beforeCreate: function(user, options) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+      }
+    }
+  });
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  }
   User.associate = function(models) {
     // associations can be defined here
-  };
+    User.hasMany(models.Trip, {
+      onDelete: "cascade"
+    });
+  }
   return User;
 };
