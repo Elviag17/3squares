d70fbeaeceb42ea51bd65d3f5506daeb9a52d23d
Elvia Gutierrez <elviagutierrez07@gmail.com>
2019-07-12 21:10:38 -0700

    adding user view

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,15 +1,21 @@
-var db = require('../models');
+var db = require("../models");
 
 //this is the users_controller.js file
 exports.registrationPage = function(req, res) {
-  res.render('users/registration', {
-    layout: 'main-registration'
+  res.render("users/registration", {
+    layout: "main-registration"
+  });
+};
+
+exports.userView = function(req, res) {
+  res.render("users/userView", {
+    layout: "main"
   });
 };
 
 exports.signOutUser = function(req, res) {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 };
 
 // login
@@ -17,12 +23,12 @@ exports.loginUser = function(req, res) {
   // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
   // So we're sending the user back the route to the members page because the redirect will happen on the front end
   // They won't get this or even be able to access this page if they aren't authed
-  res.json('/');
+  res.json("/");
 };
 
 // register a user
 exports.signUpUser = function(req, res) {
-  console.log('Controller--------------------------------');
+  console.log("Controller--------------------------------");
   console.log(req.body);
   db.User.findAll({
     where: { email: req.body.email }
@@ -44,8 +50,8 @@ exports.signUpUser = function(req, res) {
         password: req.body.password
       })
         .then(function() {
-          console.log('AFTER INSERT----------------------');
-          res.send({ redirect: '/' });
+          console.log("AFTER INSERT----------------------");
+          res.send({ redirect: "/" });
         })
         .catch(function(err) {
           res.json(err);
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var passport = require('../config/passport');
-var users_controller = require('../controllers/users_controller');
-var users_controller_vendor = require('../controllers/users_controller_vendor');
-var users_controller_distributor = require('../controllers/users_controller_distributor');
-var isAuthenticated = require('../config/middleware/isAuthenticated');
+var passport = require("../config/passport");
+var users_controller = require("../controllers/users_controller");
+var users_controller_vendor = require("../controllers/users_controller_vendor");
+var users_controller_distributor = require("../controllers/users_controller_distributor");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-router.get('/signup', users_controller.registrationPage);
+router.get("/signup", users_controller.registrationPage);
 
-router.get('/vendorregistration', users_controller_vendor.registrationPage);
+router.get("/vendorregistration", users_controller_vendor.registrationPage);
 
-router.get('/distregistration', users_controller_distributor.registrationPage);
+router.get("/distregistration", users_controller_distributor.registrationPage);
 
-router.get('/sign-out', users_controller.signOutUser);
+router.get("/sign-out", users_controller.signOutUser);
 
 router.post(
-  '/login',
-  passport.authenticate('local'),
+  "/login",
+  passport.authenticate("local"),
   users_controller.loginUser
 );
 
-router.post('/signup', users_controller.signUpUser);
+router.post("/signup", users_controller.signUpUser);
 
 // router.post('/signupvendor', users_controller_vendor.signUpUser);
 
